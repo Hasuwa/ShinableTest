c00e9ee838c617be1b43efd7688e312ecbf691b7
mkanda999 <Mamoru.kanda@jpd.co.jp>
2024-07-09 09:47:17 +0900

    import ChromeOptions

--- src/main/java/scenario/TestBase.java
+++ src/main/java/scenario/TestBase.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBase {
